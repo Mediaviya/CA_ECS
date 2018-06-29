bb6f87a942301bdb0803d709ea06105184d83fba
Mediaviya <mediaviya@hotmail.com>
2018-06-29 06:52:44 +0000

    hahah

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/apmia/apmia-ca-installer.sh start && telnet 13.0.2.196 5001 && ping localhost;
+/opt/apmia/apmia-ca-installer.sh start && tail -f /opt/apmia/logs/IntroscopeAgent.log;
